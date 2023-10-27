2fd3dc5c3902c185d19ae500df899466111913f0
Oscar Rodriguez <omrodriguez@hermosillo.tecnm.mx>
2023-10-27 12:12:48 -0700

    Versión BigCard con estilos

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
           useMaterial3: true,
         ),
         home: const MyHomePage(),
@@ -39,16 +39,46 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     return Scaffold(
-      body: Column(
-        children: [
-          Text("Idea aleatoria"),
-          Text(appState.current.asLowerCase),
-          ElevatedButton(
-            onPressed: () {
-              appState.getSiguiente();
-            }, 
-            child: Text("Siguiente")),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(idea: appState.current),
+            SizedBox(height: 20,),
+            ElevatedButton(
+              onPressed: () {
+                appState.getSiguiente();
+              }, 
+              child: Text("Siguiente")),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  final WordPair idea;
+  const BigCard({
+    super.key,
+    required this.idea 
+  });
+  @override
+  Widget build(BuildContext context) {
+    final tema = Theme.of(context);
+    final textStyle = tema.textTheme.displayMedium!.copyWith(
+      color: tema.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: tema.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          idea.asLowerCase, 
+          style: textStyle,
+          semanticsLabel: "${idea.first} ${idea.second}",
+        ),
       ),
     );
   }
