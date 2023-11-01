9a7b58423def044d4473b3b3b6e186e6025646c7
Oscar Rodriguez <omrodriguez@hermosillo.tecnm.mx>
2023-11-01 11:28:59 -0700

    Se agregó favoritos como Placeholder

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,14 @@ class _MyHomePageState extends State<MyHomePage> {
   var selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
+    Widget page;
+    
+    switch(selectedIndex) {
+      case 0: page = GeneratorPage(); break;
+      case 1: page = Placeholder(); break;
+      default:
+        throw UnimplementedError('No hay un widget para: $selectedIndex');
+    }
 
     return Scaffold(
       body: Row(
@@ -79,7 +87,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Expanded(
             child: Container(
               color: Theme.of(context).colorScheme.primaryContainer,
-              child: GeneratorPage(),)),
+              child: page,)),
         ],)
     );
   }
